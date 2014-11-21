4a1788c47d352c1f7dab4a6b191b531a93d70bc3
kkm <kmight1042@naver.com>
2014-11-21 11:21:19 +0900

    add test.js file

---
+++ test.js
@@ -0,0 +1,14 @@
+function test(){	
+
+	var num=i*j;
+		      for (var i=2;i<10;i++)
+		         innerHTML(i+"´Ü"+"\t");
+		      innerHTML();
+		      
+		      for (var i =1; i<=9; i++){
+		         for(var j=2;j<=9;j++)
+		            innerHTML(j + "*" +i+"="+(num+"\t"));
+		         innerHTML();
+		      }
+		   }
+
