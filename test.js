57633b82e1f438ac5a701582ea89eca6fb8bc59b
kkm <kmight1042@naver.com>
2014-11-21 11:52:32 +0900

    add test.js file

--- test.js
+++ test.js
@@ -2,13 +2,13 @@ function test(){
 
 	var num=i*j;
 		      for (var i=2;i<10;i++)
-		         innerHTML(i+"´Ü"+"\t");
-		      innerHTML();
+		    	  document.write(i+"´Ü"+"\t");
+		      document.write();
 		      
 		      for (var i =1; i<=9; i++){
 		         for(var j=2;j<=9;j++)
-		            innerHTML(j + "*" +i+"="+(num+"\t"));
-		         innerHTML();
+		            document.write(j + "*" +i+"="+(num+"\t"));
+		         document.write();
 		      }
 		   }
 
